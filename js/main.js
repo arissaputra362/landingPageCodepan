7773a774072a1911ba880cc91b2548bfde591f4f
Aris Saputra <arissaputra362@gmail.com>
2021-03-02 14:14:24 +0700

    update 4

--- js/main.js
+++ js/main.js
@@ -12,22 +12,25 @@ const moveMenuNav = event => {
 }
 // End of Script for active navigation
 
-window.onscroll = function() {myFunction()};
+function topNavBar(){
+	window.onscroll = function() {myFunction()};
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
-const mt = document.querySelector("#mt");
+	// Get the navbar
+	var navbar = document.getElementById("navbar");
+	const mt = document.querySelector("#mt");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+	// Get the offset position of the navbar
+	var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-	mt.classList.add("margin-top");
-  } else {
-    navbar.classList.remove("sticky");
-	mt.classList.remove("margin-top");
-  }
+	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+	function myFunction() {
+		if (window.pageYOffset >= sticky) {
+			navbar.classList.add("sticky");
+			mt.classList.add("margin-top");
+		} else {
+			navbar.classList.remove("sticky");
+			mt.classList.remove("margin-top");
+		}
+	}
 }
+
